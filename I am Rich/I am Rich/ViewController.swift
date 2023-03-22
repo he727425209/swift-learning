bd6b234a78e27115789a3cd0fe7e24161bca2b41
Kevin He <he727425209@gmail.com>
2023-03-21 20:48:34 -0400

    setup

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kevin He on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
